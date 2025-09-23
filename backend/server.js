25eedd91786362d7c3bc6c5cd4924c109e33d8ca
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-09-24 02:24:21 +0530

    pending work

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenvx.config('.env')
 const port = process.env.PORT
 
 app.use(cors({
-    origin: 'https://blogproject-mernstack-3.onrender.com',
+    origin: 'http://localhost:5173',
     credentials:true,
     methods:['GET', 'POST', 'PUT', 'DELETE'],
 }))
